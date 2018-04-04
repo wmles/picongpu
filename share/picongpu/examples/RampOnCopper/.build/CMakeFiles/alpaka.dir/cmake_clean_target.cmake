11eefec3f25ddbcc8175c70ba90c862659c0dce4
Ilja Göthel <ilja.goethel@arcor.de>
2018-04-04 11:38:07 +0200

    .build-ordner in den paramsets gelöäscht

--- share/picongpu/examples/RampOnCopper/.build/CMakeFiles/alpaka.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libalpaka.a"
-)
--- share/picongpu/examples/RampOnLCT/.build/CMakeFiles/cupla.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcupla.pdb"
-  "libcupla.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cupla.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
